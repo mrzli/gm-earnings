d171dfea1d1e9164b648a2fd1588dbaec19e3140
Goran Mržljak <goran.mrzljak@gmail.com>
2021-02-23 12:07:06 +0100

    Update personal expenses data

--- src/data/personal-expenses-data.ts
+++ src/data/personal-expenses-data.ts
@@ -8,12 +8,183 @@ import { PersonalExpensesSectionInputData } from '../types/personal-expenses/per
 export const DEFAULT_PERSONAL_EXPENSES_SECTION_INPUT_DATA: PersonalExpensesSectionInputData = {
   items: [
     {
-      name: 'Accountant Fee',
+      name: 'Rent',
       amount: {
-        amount: '400.00',
+        amount: '2300.00',
         currency: CurrencySelection.HRK
       },
       interval: ExpenseInterval.Monthly,
+      quantity: 1
+    },
+    {
+      name: 'Utilities',
+      amount: {
+        amount: '700.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Monthly,
+      quantity: 1
+    },
+    {
+      name: 'Utilities (additional per year)',
+      amount: {
+        amount: '380.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Yearly,
+      quantity: 1
+    },
+    {
+      name: 'Personal Trainer',
+      amount: {
+        amount: '180.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Weekly,
+      quantity: 3
+    },
+    {
+      name: 'Gym Membership',
+      amount: {
+        amount: '200.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Monthly,
+      quantity: 1
+    },
+    {
+      name: "Wok 'n' Walk Orders",
+      amount: {
+        amount: '69.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Weekly,
+      quantity: 3
+    },
+    {
+      name: 'Lunch.hr Orders',
+      amount: {
+        amount: '39.50',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Weekly,
+      quantity: 5
+    },
+    {
+      name: 'Netflix',
+      amount: {
+        amount: '9.99',
+        currency: CurrencySelection.EUR
+      },
+      interval: ExpenseInterval.Monthly,
+      quantity: 1
+    },
+    {
+      name: 'Youtube Premium',
+      amount: {
+        amount: '71.99',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Monthly,
+      quantity: 1
+    },
+    {
+      name: 'Gas (estimate)',
+      amount: {
+        amount: '600.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Monthly,
+      quantity: 1
+    },
+    {
+      name: 'Protein Bars',
+      amount: {
+        amount: '7.90',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Daily,
+      quantity: 1
+    },
+    {
+      name: 'Dance Classes',
+      amount: {
+        amount: '800.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Monthly,
+      quantity: 1
+    },
+    {
+      name: 'Weekend Food Orders (estimate)',
+      amount: {
+        amount: '120.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Weekly,
+      quantity: 1
+    },
+    {
+      name: 'Highway Tolls (estimate)',
+      amount: {
+        amount: '80.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Monthly,
+      quantity: 1
+    },
+    {
+      name: 'Mobile Subscription (estimate)',
+      amount: {
+        amount: '100.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Monthly,
+      quantity: 1
+    },
+    {
+      name: 'Internet Subscription',
+      amount: {
+        amount: '179.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Monthly,
+      quantity: 1
+    },
+    {
+      name: 'JetBrains Package',
+      amount: {
+        amount: '186.25',
+        currency: CurrencySelection.EUR
+      },
+      interval: ExpenseInterval.Yearly,
+      quantity: 1
+    },
+    {
+      name: 'Supplementary Health Insurance',
+      amount: {
+        amount: '70.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Monthly,
+      quantity: 1
+    },
+    {
+      name: 'Pan-Pek Protein Crackers',
+      amount: {
+        amount: '14.50',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Monthly,
+      quantity: 7
+    },
+    {
+      name: 'Therapy',
+      amount: {
+        amount: '150.00',
+        currency: CurrencySelection.HRK
+      },
+      interval: ExpenseInterval.Weekly,
       quantity: 1
     }
   ]
